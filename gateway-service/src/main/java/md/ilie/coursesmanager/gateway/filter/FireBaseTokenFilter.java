f0bd6f11f2014efaa8260b22bcf46d33f5574349
Ilie Babcenco <ilie.babcenco@amdaris.com>
2021-11-13 09:35:10 +0200

    Add feign dependency, update fb token

--- gateway-service/src/main/java/md/ilie/coursesmanager/gateway/filter/FireBaseTokenFilter.java
+++ gateway-service/src/main/java/md/ilie/coursesmanager/gateway/filter/FireBaseTokenFilter.java
@@ -4,9 +4,13 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthException;
 import com.google.firebase.auth.FirebaseToken;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 import org.springframework.web.server.ResponseStatusException;
+
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -20,7 +24,6 @@ public class FireBaseTokenFilter extends OncePerRequestFilter {
     throws ServletException, IOException {
     String authenticationHeader = request.getHeader("Authorization");
 
-    System.out.println(authenticationHeader);
     if (authenticationHeader == null || !authenticationHeader.startsWith("Bearer "))
       throw new ResponseStatusException(HttpStatus.UNAUTHORIZED,"Missing token!");
 
@@ -28,8 +31,7 @@ public class FireBaseTokenFilter extends OncePerRequestFilter {
     try {
       String token = authenticationHeader.substring(7);
       decodedToken = FirebaseAuth.getInstance().verifyIdToken(token);
-    }
-    catch (FirebaseAuthException e) {
+    } catch (FirebaseAuthException e) {
       throw new ResponseStatusException(HttpStatus.UNAUTHORIZED,"Error! "+e.getMessage());
     }
 
@@ -37,6 +39,17 @@ public class FireBaseTokenFilter extends OncePerRequestFilter {
       throw new ResponseStatusException(HttpStatus.UNAUTHORIZED,"Invalid token!");
     }
 
+    Authentication auth = getAuthentication(decodedToken);
+    SecurityContextHolder.getContext().setAuthentication(auth);
+    logger.debug("Successfully Authenticated");
+
     filterChain.doFilter(request,response);
   }
+
+  private Authentication getAuthentication(FirebaseToken decodedToken) {
+    assert decodedToken != null;
+    //FirebaseAuthenticationToken not found
+    return new UsernamePasswordAuthenticationToken(decodedToken.getUid(), decodedToken);
+  }
+
 }
