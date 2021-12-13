3a021ae986cf766dfee2fa03ce7fa8a80db3aac8
Ilie Babcenco <ilie.babcenco@amdaris.com>
2021-12-13 20:06:23 +0200

    Update user-service to handle register endpoint with no token

--- users-service/src/main/java/md/ilie/coursesmanager/userservice/config/firebase/FirebaseTokenFilter.java
+++ users-service/src/main/java/md/ilie/coursesmanager/userservice/config/firebase/FirebaseTokenFilter.java
@@ -4,14 +4,13 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthException;
 import com.google.firebase.auth.FirebaseToken;
 import lombok.AllArgsConstructor;
-import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
+import org.springframework.util.AntPathMatcher;
 import org.springframework.web.filter.OncePerRequestFilter;
 import org.springframework.web.server.ResponseStatusException;
-
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -22,30 +21,35 @@ import java.io.IOException;
 @AllArgsConstructor
 public class FirebaseTokenFilter extends OncePerRequestFilter {
 
-    @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
-            throws ServletException, IOException {
-        String authenticationHeader = request.getHeader("Authorization");
-        if (authenticationHeader == null || !authenticationHeader.startsWith("Bearer "))
-            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Missing token!");
-        FirebaseToken decodedToken = null;
-        try {
-            String token = authenticationHeader.substring(7);
-            decodedToken = FirebaseAuth.getInstance().verifyIdToken(token);
-        } catch (FirebaseAuthException e) {
-            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Error! " + e.getMessage());
-        }
-        if (decodedToken == null) {
-            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Invalid token!");
-        }
-        Authentication auth = getAuthentication(decodedToken);
-        SecurityContextHolder.getContext().setAuthentication(auth);
-        filterChain.doFilter(request, response);
+  @Override
+  protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+    throws ServletException, IOException {
+    String authenticationHeader = request.getHeader("Authorization");
+    if (authenticationHeader == null || !authenticationHeader.startsWith("Bearer "))
+      throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Missing token!");
+    FirebaseToken decodedToken = null;
+    try {
+      String token = authenticationHeader.substring(7);
+      decodedToken = FirebaseAuth.getInstance().verifyIdToken(token);
+    } catch (FirebaseAuthException e) {
+      throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Error! " + e.getMessage());
     }
-
-    private Authentication getAuthentication(FirebaseToken decodedToken) {
-        assert decodedToken != null;
-        return new FirebaseAuthenticationToken(decodedToken.getEmail(), new FirebaseTokenHolder(decodedToken));
+    if (decodedToken == null) {
+      throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Invalid token!");
     }
+    Authentication auth = getAuthentication(decodedToken);
+    SecurityContextHolder.getContext().setAuthentication(auth);
+    filterChain.doFilter(request, response);
+  }
 
+  private Authentication getAuthentication(FirebaseToken decodedToken) {
+    assert decodedToken != null;
+    return new FirebaseAuthenticationToken(decodedToken.getEmail(), new FirebaseTokenHolder(decodedToken));
+  }
+
+  @Override
+  protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
+    AntPathMatcher pathMatcher = new AntPathMatcher();
+    return pathMatcher.match("/users/register", request.getServletPath());
+  }
 }
